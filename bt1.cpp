68074b36d55ccbfd1d9dee68810963e162f4c9b4
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2025-01-09 07:50:35 +0700

    Add files via upload

--- bt1.cpp
+++ bt1.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+struct SinhVien{
+	char name[100];
+	int age;
+	char phoneNumber[100];
+};
+
+typedef struct SinhVien SinhVien;
 
 int main(){
-	int n=2905;
-	int *ptr;	
-	ptr = &n;
-	printf("Gia tri cua bien duoc khai bao: %d\n",*ptr);
-	printf("Dia chi cua bien duoc khai bao: %d",ptr);
+	SinhVien x;
+	strcpy(x.name,"Diep Thanh Nhan");
+	x.age=18;
+	strcpy(x.phoneNumber,"0907615070");
+	
+	printf("Ten: %s\n",x.name);
+	printf("Tuoi: %d\n",x.age);
+	printf("Sdt: %s",x.phoneNumber);
 }
